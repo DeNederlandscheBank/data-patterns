fccc00167f88ac36a8e8c7c03ec3305eed346cbf
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2019-11-06 13:14:12 +0100

    Bump version: 0.1.1 → 0.1.1

--- data_patterns/__init__.py
+++ data_patterns/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """De Nederlandsche Bank"""
 __email__ = 'ECDB_berichten@dnb.nl'
-__version__ = '__version__ = '__version__ = '0.1.1'''
+__version__ = '__version__ = '__version__ = '__version__ = '0.1.1''''
 
 from .data_patterns import *
 from .encodings import *
