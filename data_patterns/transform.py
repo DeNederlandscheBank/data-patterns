4e454e23088def2a9a8f6dcc17d64594635acc90
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2019-11-06 13:12:25 +0100

    Bump version: 0.1.0 → 0.1.0

--- data_patterns/transform.py
+++ data_patterns/transform.py
@@ -11,7 +11,7 @@ from .constants import *
 
 __author__ = """De Nederlandsche Bank"""
 __email__ = 'ECDB_berichten@dnb.nl'
-__version__ = '__version__ = '0.1.0''
+__version__ = '__version__ = '__version__ = '0.1.0'''
 
 class PatternDataFrame(pd.DataFrame):
     ''' 
