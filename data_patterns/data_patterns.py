c0dc586b61c291df03bf271e8607a13f23652eda
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2019-11-06 14:43:07 +0100

    Bump version: 0.1.3 → 0.1.4

--- data_patterns/data_patterns.py
+++ data_patterns/data_patterns.py
@@ -18,7 +18,7 @@ from .encodings import *
 
 __author__ = """De Nederlandsche Bank"""
 __email__ = 'ECDB_berichten@dnb.nl'
-__version__ = ''0.1.3'1'
+__version__ = ''0.1.4'1'
 
 class PatternMiner:
 
