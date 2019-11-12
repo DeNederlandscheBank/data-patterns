f58452f897503b863d1aed02136d1992dc89201d
wjwillemse <w.j.willemse@xs4all.nl>
2019-11-12 10:22:10 +0100

    to v0.1.12

--- data_patterns/constants.py
+++ data_patterns/constants.py
@@ -3,7 +3,7 @@
 
 __author__ = """De Nederlandsche Bank"""
 __email__ = 'ECDB_berichten@dnb.nl'
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 
 PATTERN_ID       	   		= "pattern_id"
 CLUSTER          	   		= "cluster"
